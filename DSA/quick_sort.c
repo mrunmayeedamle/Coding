ce5bad54b1cda0c952e4d359ceccd3f42b8f7024
Mrunmayee <mrunmayee1278@gmail.com>
2023-10-31 21:09:28 +0530

    added quick_sort.c

--- DSA/quick_sort.c
+++ DSA/quick_sort.c
@@ -1,11 +1,10 @@
 //Quick sort using recursion
 #include <stdio.h>
 #include <stdlib.h>
-int array[50], n, i;
+int array[50], n, i, j, temp;
 
 int partition(int, int);
 void quickSort(int, int);
-void display();
 
 int main()
 {
@@ -24,4 +23,48 @@ int main()
         printf("%d ", array[i]);
     }
     printf("]\n");
+
+    quickSort(0, n-1);
+
+    printf("Array after sorting: [");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("]");
+
+    return 0;
+}
+
+int partition(int l, int h)
+{
+    int pivot = array[h];
+    i = (l - 1);
+
+    for(j = l; j <= h - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            temp = array[i];
+            array[i] = array[j];
+            array[j] =  temp;
+        }
+        
+    }
+    temp = array[i+1];
+    array[i+1] = array[h];
+    array[h] = temp;
+
+    return(i+1);
+}
+
+void quickSort(int low, int high)
+{
+    if(low < high)
+    {
+        int pi = partition(low, high);
+        quickSort(low, pi-1);
+        quickSort(pi+1, high);
+    }
 }
