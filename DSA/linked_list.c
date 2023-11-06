8b33af159b2a1a7ea3b53b1e5b9f41016922c737
Mrunmayee <mrunmayee1278@gmail.com>
2023-11-06 10:16:12 +0530

    modified linked_list.c

--- DSA/linked_list.c
+++ DSA/linked_list.c
@@ -6,6 +6,9 @@ void display();
 void insert_at_start();
 void insert_at_end();
 void random_insert();
+void delete_at_start();
+void delete_at_end();
+void random_delete();
 
 struct node
 {
@@ -21,7 +24,8 @@ int main()
     {
         printf("Linked list operations \n \n");
         printf("Enter the operation you want to perform: \n");
-        printf("1) Insert : At start. \n2) Insert: At end. \n3) Insert anywhere \n4) Display list \n5) Exit\n");
+        printf("1) Insert : At start. \n2) Insert: At end. \n3) Insert anywhere \n4) Delete a node at the start \n");
+        printf("5) Delete node at the end \n6) Delete node randomly \n7) Display list \n8) Exit \n");
         scanf("%d", &x);
         switch(x)
         {
@@ -31,9 +35,15 @@ int main()
             break;
             case 3: random_insert();
             break;
-            case 4: display();
+            case 4: delete_at_start();
             break;
-            case 5: exit(1);
+            case 5: delete_at_end();
+            break;
+            case 6: random_delete();
+            break;
+            case 7: display();
+            break;
+            case 8: exit(1);
             break;
             default: printf("Error! Wrong Choice. \n");
         }
@@ -143,13 +153,96 @@ void random_insert()
     }
 }
 
+void delete_at_start()
+{
+    struct node *ptr;
+    if (head == NULL)
+    {
+        printf("List is empty \n\n");
+    }
+    else
+    {
+        ptr = head;
+        head = ptr->next;
+        free(ptr);
+        printf("Node deleted from the start.\n");
+    }
+}
+
+void delete_at_end()
+{
+    struct node *ptr, *ptr_1;
+    if(head == NULL)
+    {
+        printf("List is empty\n\n");
+    }
+    else if(head->next == NULL)
+    {
+        head = NULL;
+        free(head);
+        printf("Only node in the list deleted\n\n");
+    }
+    else
+    {
+        ptr = head;
+        while(ptr->next != NULL)
+        {
+            ptr_1 = ptr;
+            ptr = ptr->next;
+        }
+        ptr_1->next = NULL;
+        free(ptr);
+        printf("Deleted node from the end\n\n");
+    }
+}
+
+void random_delete()
+{
+    int i, loc;
+    struct node *ptr, *ptr_1;
+
+    printf("Enter the position of the node after which you want to delete: \n");
+    scanf("%d", &loc);
+
+    if(loc == 0)
+    {
+        ptr = head;
+        if(ptr == NULL)
+        {
+            printf("Empty list. \n\n");
+        }
+        head = head->next;
+        free(ptr);
+        printf("Deleted first node. \n\n");
+        return;
+    }
+    else
+    {
+        ptr = head;
+        for(i = 0; i < loc; i++)
+        {
+            ptr_1 =  ptr;
+            ptr = ptr->next;
+            if(ptr == NULL)
+            {
+                printf("Delete operation cannot be performed. \n");
+                return;
+            }
+        }
+        ptr_1->next = ptr->next;
+        free(ptr);
+        printf("Deleted node at %d", loc+1);
+    }
+    
+}
+
 void display()
 {
     struct node *ptr;
     ptr = head;
     if(ptr == NULL)
     {
-        printf("Nothing to print. /n/n");
+        printf("Nothing to print. \n\n");
     }
     else
     {
@@ -159,7 +252,7 @@ void display()
             printf(" %d ", ptr->data);
             ptr = ptr->next;
         }
-    }
     printf("] \n");
+    }
     sleep(1);
 }
