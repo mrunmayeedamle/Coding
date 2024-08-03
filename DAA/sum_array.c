e3b4ab7400b9b5dce6684e5ce35ebe8b70071420
Mrunmayee Damle <113115806+mrunmayeedamle@users.noreply.github.com>
2024-08-03 16:23:27 +0530

    Add files via upload

---
+++ DAA/sum_array.c
@@ -0,0 +1,41 @@
+# include <stdio.h>
+# include <time.h>
+
+int sum_arr(int[], int);
+
+int main()
+{
+    clock_t begin = clock();
+
+    int num, sum, i;
+    int array[100];
+
+    printf("Enter the number of elements in array: ");
+    scanf("%d", &num);
+
+    for(i = 0; i < num; i++)
+    {
+        printf("Enter element %d: ", (i+1));
+        scanf("%d", &array[i]);
+    }
+
+    sum = sum_arr(array, num);
+    printf("Sum of elements in array is %d\n\n", sum);
+
+    clock_t end = clock();
+
+    double time = (double)(end - begin) / CLOCKS_PER_SEC;
+
+    printf("Time taken to execute: %f", time);
+    return 0;
+}
+
+int sum_arr(int arr[], int n)
+{
+    int i, s = 0;
+    for (i = 0; i < n; i++)
+    {
+        s = s + arr[i];
+    }
+    return s;
+}
