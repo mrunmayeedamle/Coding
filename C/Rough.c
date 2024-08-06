bec0a8f268881190599fc8541b3768ad2a9254b6
Mrunmayee <mrunmayee1278@gmail.com>
2024-08-06 22:28:05 +0530

    some changes to code

--- C/Rough.c
+++ C/Rough.c
@@ -1,27 +1,14 @@
-#include <stdio.h>
-int *arr_fun(int*);
+# include <stdio.h>
 
 int main()
 {
-    int *array;
-    int a[10];
-    int i;
-    array = arr_fun(a);
-    printf("Elements are: \n");
-    for(i = 0; i < 5; i++)
+    int a = 6;
+    int b = 0;
+    while(a < 10)
     {
-        printf("%d\t",array[i]);
+        a = a/12 + 1;
+        a += b;
     }
+    printf("%d", a);
     return 0;
-}
-
-int *arr_fun(int *arr)
-{
-    int i, n = 5;
-    printf("Enter the elements: \n");
-    for(i = 0; i <n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    return arr;
 }
